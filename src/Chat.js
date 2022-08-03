d0eb04f6392a0a158739dee4bfaab7246f48910b
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-03 17:05:29 -0300

    connection established.

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:8080");
+socket.on("connect", () =>
+  console.log("[IO] Connect => A new connection has been established")
+);
 
 const Chat = () => {
   const [message, updateMessage] = useState("");
